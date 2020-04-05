c27a7d786889b8aca3e62db3b65e2eff8d8c81b3
Banjo Obayomi <banjtheman@gmail.com>
2020-04-05 17:12:00 -0400

    added pair plot, fasttext, and umap

--- grim/spells/__init__.py
+++ grim/spells/__init__.py
@@ -4,6 +4,8 @@ from .holy.heal import spell as heal
 #Acrane Spells
 from .arcane.rf_reg import spell as rf_reg
 from .arcane.svm_classifier import spell as svm_classifier
+from .arcane.umap_grim import spell as umap_grim
+from .arcane.fasttext_grim import spell as fasttext_grim
 
 #Nature Spells
 from .nature.histogram import spell as histogram
@@ -13,6 +15,7 @@ from .nature.bar_chart import spell as bar_chart
 from .nature.line_chart import spell as line_chart
 from .nature.area_chart import spell as area_chart
 from .nature.stacked_bar import spell as stacked_bar
+from .nature.pair_plot import spell as pair_plot
 
 #Air spells
 from .air.plot_map import spell as plot_map
@@ -33,5 +36,8 @@ REGISTERED_SPELLS = dict(
     stacked_bar = stacked_bar,
     svm_classifier= svm_classifier,
     plot_map = plot_map,
-    time_bar_chart = time_bar_chart
+    time_bar_chart = time_bar_chart,
+    pair_plot = pair_plot,
+    umap_grim = umap_grim,
+    fasttext_grim = fasttext_grim
 )
